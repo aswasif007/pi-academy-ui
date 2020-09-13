0ff3bf0bf55233ffc66f2f3f2787e8ea3d5c5344
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-09-13 22:51:38 +0600

    Add moment and fromNow pipe

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import ClickOutside from 'vue-click-outside'
+import moment from 'moment';
 
 Vue.directive('offclick', ClickOutside)
+Vue.filter('fromNow', value => {
+  const datetime = new Date(value);
+  return moment(datetime).fromNow();
+})
 
 Vue.config.productionTip = false
 
