30dbc8f77754afe0d8d7d42f856bdc313b991aca
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-09-13 20:44:41 +0600

    Add People and TextContent component

---
+++ src/library/People.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="lib-people" :class="size">
+      <Avatar class="avatar" :imgSrc="imgSrc" />
+      <div class="info">
+        <p class="name">John Doe</p>
+        <p v-if="category" class="category">{{ category }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from '@/library/Avatar';
+
+export default {
+  props: {
+    imgSrc: { type: String },
+    name: { type: String, required: true },
+    category: { type: String },
+    size: { validator: val => ['sm', 'md', 'lg'].includes(val), default: 'md' },
+  },
+  components: {
+    Avatar,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.lib-people {
+  display: inline-flex;
+
+  p {
+    margin: 0;
+    line-height: 1;
+  }
+
+  &.sm > .avatar {
+    height: var(--l);
+    width: var(--l);
+  }
+
+  &.md > .avatar {
+    height: var(--xxl);
+    width: var(--xxl);
+  }
+
+  &.lg > .avatar {
+    height: var(--xxxxl);
+    width: var(--xxxxl);
+  }
+
+  .info {
+    font-family: 'PT Serif';
+    @include position-center;
+    text-align: left;
+    margin-left: 8px;
+  }
+  
+  .name {
+    color: var(--color2);
+  }
+
+  .category {
+    color: var(--color4);
+    margin-top: 2px;
+  }
+
+  &.sm {
+    .name {
+      font-size: var(--xxs);
+    }
+
+    .category {
+      font-size: var(--xxxs);
+    }
+  }
+
+  &.md {
+    .name {
+      font-size: var(--xs);
+    }
+
+    .category {
+      font-size: calc(var(--xxs) - 2px);
+    }
+  }
+
+  &.lg {
+    .name {
+      font-size: var(--m);
+    }
+
+    .category {
+      font-size: var(--xxs);
+    }
+  }
+}
+</style>
---
+++ src/library/TextContent.vue
@@ -0,0 +1,47 @@
+<template>
+  <p class="lib-text-content" :class="size">
+    <slot />
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    size: { validator: val => ['xsm', 'sm', 'md', 'lg', 'xlg'].includes(val), default: 'md' }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.lib-text-content {
+  display: block;
+  text-align: justify;
+  font-family: 'Quattrocento Sans';
+  color: var(--color3);
+
+  &.xsm {
+    font-size: var(--xxxs);
+    margin: 0 0 var(--xxxxs) 0;
+  }
+
+  &.sm {
+    font-size: var(--xxs);
+    margin: 0 0 var(--xxxxs) 0;
+  }
+
+  &.md {
+    font-size: var(--xs);
+    margin: 0 0 var(--xxxs) 0;
+  }
+
+  &.lg {
+    font-size: var(--s);
+    margin: 0 0 var(--xxxs) 0;
+  }
+
+  &.xlg {
+    font-size: var(--m);
+    margin: 0 0 var(--xxs) 0;
+  }
+}
+</style>
