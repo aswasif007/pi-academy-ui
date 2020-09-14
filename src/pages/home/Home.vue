69b1b6d4a999047029f9479c3b39ec0c44a3b2c8
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-09-14 20:26:20 +0600

    Add homepage

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home-page">
-    Home
+    <DiscussionInput
+      class="new-post"
+      placeholder="Post something..."
+      actionLabel="Post"
+    />
+    <hr class="line">
+    <div>
+      <DiscussionThread v-for="thread in threads" :key="thread.guid"
+        class="thread"
+        :discussion="thread"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import DiscussionInput from '@/library/DiscussionInput';
+import DiscussionThread from '@/library/DiscussionThread';
 
+export default {
+  components: {
+    DiscussionInput,
+    DiscussionThread,
+  },
+  mounted() {
+    this.$store.discussions.dispatch('reloadHomeThreads');
+  },
+  computed: {
+    threads() {
+      return this.$store.discussions.getters.homeThreads;
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .home-page {
-  display: flex;
-  height: 200vh;
+  padding: var(--m);
+
+  .line {
+    margin: var(--xxs) 0;
+  }
+
+  .thread {
+    margin-top: var(--xl);
+  }
+}
+
+@media only screen and (max-width: $small-screen-breakpoint) {
+  .home-page {
+    padding: var(--xxxxs);
+  }
 }
 </style>
