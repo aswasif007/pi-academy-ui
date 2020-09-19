d28fdddf2065a717c4db1971a037d24cb3b27524
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2020-09-19 22:12:44 +0600

    Add build artifect

---
+++ docs/precache-manifest.131b820458b656e687c62a5ff995f416.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6161950a4a7099674e48",
+    "url": "css/app.3cf1fa5f.css"
+  },
+  {
+    "revision": "5f7303c0e7f09925586e218ab8fd9b19",
+    "url": "fonts/PTSerif-Regular.5f7303c0.ttf"
+  },
+  {
+    "revision": "e1507baeb71830b408ba07c916be886c",
+    "url": "fonts/QuattrocentoSans-Regular.e1507bae.ttf"
+  },
+  {
+    "revision": "dc79ca16b32ffad9991ab99a05da471a",
+    "url": "fonts/RedRose-Regular.dc79ca16.ttf"
+  },
+  {
+    "revision": "d720c24b6ab86aa9e2cf637f9acb577e",
+    "url": "index.html"
+  },
+  {
+    "revision": "6161950a4a7099674e48",
+    "url": "js/app.965dc13c.js"
+  },
+  {
+    "revision": "1958bedbcf1faacadae3",
+    "url": "js/chunk-199fac64.2e494872.js"
+  },
+  {
+    "revision": "5dfa84951365202c8b9e",
+    "url": "js/chunk-21434d28.0b05d74a.js"
+  },
+  {
+    "revision": "df0a87190b7f762dbba3",
+    "url": "js/chunk-589c650c.290dbb9a.js"
+  },
+  {
+    "revision": "08d84c118263002268bd",
+    "url": "js/chunk-5d03d3ef.3920345e.js"
+  },
+  {
+    "revision": "871d0512cc4d02c9455e",
+    "url": "js/chunk-a3fae374.141282d4.js"
+  },
+  {
+    "revision": "23e0eb5f7b5ed1fa1d1d",
+    "url": "js/chunk-f429e010.9be61bf0.js"
+  },
+  {
+    "revision": "1777e6166135f1300485",
+    "url": "js/chunk-vendors.a13ecf60.js"
+  },
+  {
+    "revision": "35f4cf9ffa4f05ede10c135214a4a7b2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
